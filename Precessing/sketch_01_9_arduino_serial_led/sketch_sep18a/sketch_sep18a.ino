fc964b3c4a8a08e7528fdde5253676e00b08322c
ygim98710 <55430276+ygim98710@users.noreply.github.com>
2019-09-18 23:25:50 +0900

    Update sketch_sep18a.ino
    After comment

--- Precessing/sketch_01_9_arduino_serial_led/sketch_sep18a/sketch_sep18a.ino
+++ Precessing/sketch_01_9_arduino_serial_led/sketch_sep18a/sketch_sep18a.ino
@@ -1,17 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
+  //LED_BUILTN : 내부적으로 13번으로 설정되어있음
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //버퍼에 읽은 바이트 수 반환
   if (Serial.available() > 0) {
-    // get incoming byte:
+    //한 바이트 읽어옴
     int inByte = Serial.read();
 
+    //0이면 끄고 1이면 켜라
     if(inByte=='0')
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(LED_BUILTIN, LOW);
     else if(inByte=='1')
       digitalWrite(LED_BUILTIN, HIGH); 
   }
